64269cd04b31cef515ff08631fd67ad626c984d0
reyanshm2021 <mauryageeta323@gmail.com>
2024-11-08 13:15:55 +0530

    Create app.go

---
+++ app.go
@@ -0,0 +1,21 @@
+# app.go
+
+package main
+
+import (
+    "fmt"
+    "time"
+    "os/user"
+)
+
+func main () {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+
+    for {
+        fmt.Println("user: " + user.Username + " id: " + user.Uid)
+        time.Sleep(1 * time.Second)
+    }
+}
